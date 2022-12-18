7f21489b5a2d3eb1ad62aa42b54f833c5ea3fc5f
Sharon Mary <116333051+SharonMaryCharles@users.noreply.github.com>
2022-12-18 12:21:53 -0500

    Data - index.js and posts.js
    Data - index.js and posts.js

---
+++ data/index.js
@@ -0,0 +1,14 @@
+const userData = require("./users");
+const postData = require("./posts");
+const commentData = require("./comments");
+const reportData = require("./reports");
+const bmicalculator = require("./bmiCalculator");
+
+module.exports = 
+{
+    users: userData,
+    posts: postData,
+    comments: commentData,
+    reports: reportData,
+    bmiCalculator: bmicalculator
+};
